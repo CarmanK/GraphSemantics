b5bc339cafa69e780895456f3520c2b976204055
Kevin Carman <36905432+CarmanK@users.noreply.github.com>
2019-06-13 17:35:19 -0400

    made the scraper more easily scalable and changed the output to a file. errors should be fixed now too

--- scraper.py
+++ scraper.py
@@ -1,10 +1,8 @@
 from html_requests import html_get
 from bs4 import BeautifulSoup
 
-fixedpoint_index = 1
-
-layer = "top"
-urls = [
+# Top
+layer1 = [
     "https://www.rcsb.org/structure/1j0o",
     "https://www.rcsb.org/structure/3mdt",
     "https://www.rcsb.org/structure/2r4x",
@@ -13,52 +11,57 @@ urls = [
     "https://www.rcsb.org/structure/4rkm"
 ]
 
-# layer = "mid"
-# urls = [
-#     "https://www.rcsb.org/structure/1yot",
-#     "https://www.rcsb.org/structure/3hhb",
-#     "https://www.rcsb.org/structure/1gli",
-#     "https://www.rcsb.org/structure/1y85",
-#     "https://www.rcsb.org/structure/2hhe",
-#     "https://www.rcsb.org/structure/1y45",
-#     "https://www.rcsb.org/structure/1a3n",
-#     "https://www.rcsb.org/structure/4hhb",
-#     "https://www.rcsb.org/structure/1y0b",
-#     "https://www.rcsb.org/structure/1xz2",
-#     "https://www.rcsb.org/structure/1y2z",
-#     "https://www.rcsb.org/structure/2hhb",
-#     "https://www.rcsb.org/structure/1bab"
-# ]
+# Middle
+layer2 = [
+    "https://www.rcsb.org/structure/1yot",
+    "https://www.rcsb.org/structure/3hhb",
+    "https://www.rcsb.org/structure/1gli",
+    "https://www.rcsb.org/structure/1y85",
+    "https://www.rcsb.org/structure/2hhe",
+    "https://www.rcsb.org/structure/1y45",
+    "https://www.rcsb.org/structure/1a3n",
+    "https://www.rcsb.org/structure/4hhb",
+    "https://www.rcsb.org/structure/1y0b",
+    "https://www.rcsb.org/structure/1xz2",
+    "https://www.rcsb.org/structure/1y2z",
+    "https://www.rcsb.org/structure/2hhb",
+    "https://www.rcsb.org/structure/1bab"
+]
 
-# layer = "bot"
-# urls = [
-#     "https://www.rcsb.org/structure/4u9b",
-#     "https://www.rcsb.org/structure/2z47",
-#     "https://www.rcsb.org/structure/2vr0",
-#     "https://www.rcsb.org/structure/2r4y",
-#     "https://www.rcsb.org/structure/3mdm"
-# ]
+# Bottom
+layer3 = [
+    "https://www.rcsb.org/structure/4u9b",
+    "https://www.rcsb.org/structure/2z47",
+    "https://www.rcsb.org/structure/2vr0",
+    "https://www.rcsb.org/structure/2r4y",
+    "https://www.rcsb.org/structure/3mdm"
+]
 
-output = []
-for i in range(len(urls)):
-    raw_html = html_get(urls[i])
-    if raw_html is not None:
-        html = BeautifulSoup(raw_html, 'html.parser')
+layers = [layer1, layer2, layer3]
+with open('scrapedText.txt', 'w') as file:
+    for i in range(len(layers)):
+        for j in range(len(layers[i])):
+            raw_html = html_get(layers[i][j])
+            if raw_html is not None:
+                html = BeautifulSoup(raw_html, 'html.parser')
 
-        html_titles = html.find(id = "primarycitation")
-        if html_titles is not None:
-            title = html_titles.select('h4')[0].text
-        else:
-            title = ""
-            print("Error finding title data at {0}".format(urls[i]))
+                html_titles = html.find(id = "primarycitation")
+                if html_titles is not None:
+                    title = html_titles.select('h4')[0].text
+                else:
+                    title = ""
+                    print("Error finding title data at {0}".format(layers[i][j]))
 
-        html_paragraphs = html.find(id = "abstractFull")
-        if html_paragraphs is not None:
-            abstract = html_paragraphs.select('p')[0].text
-        else:
-            abstract = ""
-            print("Error finding abstract data at {0}".format(urls[i]))
+                html_paragraphs = html.find(id = "abstractFull")
+                if html_paragraphs is not None:
+                    abstract = html_paragraphs.select('p')[0].text
+                else:
+                    abstract = ""
+                    print("Error finding abstract data at {0}".format(layers[i][j]))
 
-        output.append((fixedpoint_index, layer, urls[i], title, abstract))
-    else:
-        print("Error scraping data from {0}".format(urls[i]))
+                file.write(str(i + 1))
+                file.write("\n" + title + "\n")
+                file.write(abstract + "\n")
+            else:
+                print("Error scraping data from {0}".format(layers[i][j]))
+    file.write("EOF")
