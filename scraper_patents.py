c1ce4c23a484638278af3311fd26202462a0561e
CarmanK <carmank@etown.edu>
2019-07-17 20:49:45 -0400

    omgomgomgomg i think i finally got google api to work enough to be usable

--- scraper_patents.py
+++ scraper_patents.py
@@ -15,44 +15,50 @@ def main():
     if not os.path.exists('output_data/tmp'):
         os.mkdir('output_data/tmp')
 
-    url = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect2=PTO1&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&d=PALL&RefSrch=yes&Query=PN%2F'
+    url = 'https://patents.google.com/patent/US'
+    url2 = '/en?oq='
 
     with open('./input_data/patent_ids.txt', 'r') as input_file:
         patent_ids = input_file.readlines()
     
     with open('./output_data/tmp/scraped_patent_text.txt', 'w') as output_file:
         for id in range(0, 10000):
-            raw_html = html_get(patent_ids[id], url)
+            raw_html = html_get(patent_ids[id], url, url2)
             if raw_html is not None:
                 html = BeautifulSoup(raw_html, 'html.parser')
-
-                abstract = html.find_all(['p'])[0].get_text()
-                if abstract[:21] != '  Current U.S. Class:':
-                    # Clean up the abstract
-                    abstract = abstract.replace('\n', '')
-                    abstract = abstract.replace('    ', '')
-                    output_file.write(abstract + '\n')
+                divs = html.findAll("div", {"class": "abstract"})
+                if divs:
+                    output_file.write(divs[0].get_text() + '\n')
                 else:
-                    print('The abstract of ID "' + patent_ids[id][:-1] + '" could not be found.')
+                    print(patent_ids[id])
 
-def html_get(id, url):
+                # abstract = html.find_all(['p'])[0].get_text()
+                # if abstract[:21] != '  Current U.S. Class:':
+                #     # Clean up the abstract
+                #     abstract = abstract.replace('\n', '')
+                #     abstract = abstract.replace('    ', '')
+                #     output_file.write(abstract + '\n')
+                # else:
+                #     print('The abstract of ID "' + patent_ids[id][:-1] + '" could not be found.')
+
+def html_get(id, url, url2):
     '''
     Gets the content at `url` by making an HTTP GET request.
     If the content-type of the response is HTML, return the text content, otherwise return None.
     '''
-    time.sleep(random.random())
-    session = requests.Session()
-    session.headers.update({'Host': 'patft.uspto.gov',
-                            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
-                            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                            'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
-                            'Accept-Encoding': 'gzip, deflate',
-                            'Connection': 'keep-alive',
-                            'Pragma': 'no-cache',
-                            'Cache-Control': 'no-cache'})
-    url += str(id)
+    # time.sleep(random.random())
+    # session = requests.Session()
+    # session.headers.update({'Host': 'patft.uspto.gov',
+    #                         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
+    #                         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    #                         'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+    #                         'Accept-Encoding': 'gzip, deflate',
+    #                         'Connection': 'keep-alive',
+    #                         'Pragma': 'no-cache',
+    #                         'Cache-Control': 'no-cache'})
+    url += (id[:-1] + url2 + id[:-1])
     try:
-        with closing(session.get(url, stream = True)) as response:
+        with closing(requests.get(url, stream = True)) as response:
             if is_html(response):
                 return response.content
             else:
